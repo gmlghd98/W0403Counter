48b6a96b30613b5213879b5cd023361fb121edd1
user <gmlghd98@gmail.com>
2022-09-28 14:58:31 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20170523/w0403counter/MainActivity.kt
@@ -0,0 +1,55 @@
+package kr.ac.kumoh.s20170523.w0403counter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.s20170523.w0403counter.ui.theme.W0403CounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                Counter()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit ) {
+    W0403CounterTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun Counter() {
+    Text(text = "0")
+    Button(onClick = {}) {
+        Text(text = "증가")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        Counter()
+    }
+}
